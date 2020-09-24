00565123f8e0d28b480af15fa5acee9b56860238
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2020-09-24 10:00:20 +0800

    Add Spinner and colour elements

--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.coloractivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        final String[] colour = {"Red", "Yellow", "Cyan", "Gray", "Magenta", "Silver", "Teal", "Green"};
+        ArrayAdapter<String> colourList = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item,
+                colour);spinner.setAdapter(colourList);
+
+
+
     }
 }
