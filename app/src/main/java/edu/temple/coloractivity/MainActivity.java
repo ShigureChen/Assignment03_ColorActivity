2a8e0156a399e2e897305d9c9e75b1160184dc49
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2020-09-24 10:54:48 +0800

    Add spinner on select listener

--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.coloractivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,11 +16,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spinner = (Spinner)findViewById(R.id.spinner);
+
         final String[] colour = {"Red", "Yellow", "Cyan", "Gray", "Magenta", "Silver", "Teal", "Green"};
         ArrayAdapter<String> colourList = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_spinner_dropdown_item,
-                colour);spinner.setAdapter(colourList);
+                colour);
+        spinner.setAdapter(colourList);
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView){
+                
+            }
+        });
 
 
     }
